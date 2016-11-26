f99c27a6a58d89ec17dfe0e76cd6039ff809fad6
Vo1ked <Volk.xxx@mail.ru>
2016-11-26 02:29:34 +0200

    HomeWork4 At Last Complite

---
+++ Assets/scripts/PowerChange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PowerChange : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _BowlingBall;
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Text>().text = "Score: 0 pins";
+    }
+
+// Update is called once per frame
+void Update()
+{
+    GetComponent<Text>().text = "Power: " + _BowlingBall.GetComponent<BowlingPlay>().PushingForse;
+}
+}
