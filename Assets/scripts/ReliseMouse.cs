eef3837c3c0dd0d77ea7497a0b21f95a42f1c939
Vo1ked <Volk.xxx@mail.ru>
2016-12-14 09:07:17 +0200

    CreateNewBranch Create Light, Planet Rotation&Orbit, Orbit line

--- Assets/scripts/ReliseMouse.cs
+++ Assets/scripts/ReliseMouse.cs
@@ -15,14 +15,16 @@ public class ReliseMouse : MonoBehaviour {
 	    {
 	        GetComponent<RigidbodyFirstPersonController>().mouseLook.XSensitivity = 0;
 	        GetComponent<RigidbodyFirstPersonController>().mouseLook.YSensitivity = 0;
-	        //GetComponent<RigidbodyFirstPersonController>().mouseLook.lockCursor = false;
-	    }
-	    else if (!Input.GetButton("Ctrl"))
+            GetComponent<RigidbodyFirstPersonController>().mouseLook.SetCursorLock(false);
+
+        }
+        else if (!Input.GetButton("Ctrl"))
 	    {
             GetComponent<RigidbodyFirstPersonController>().mouseLook.XSensitivity = 2;
             GetComponent<RigidbodyFirstPersonController>().mouseLook.YSensitivity = 2;
-            //GetComponent<RigidbodyFirstPersonController>().mouseLook.lockCursor = true;
+            GetComponent<RigidbodyFirstPersonController>().mouseLook.SetCursorLock(true);
+
         }
 
-	}
+    }
 }
